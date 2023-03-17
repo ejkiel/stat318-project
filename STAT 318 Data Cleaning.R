2b47f642b0f7b1deeeec37e2fd8c5fc14a883b49
Elizabeth Kiel <ek5@wellesley.edu>
2023-03-17 01:27:32 -0400

    added final categorical var

--- STAT 318 Data Cleaning.R
+++ STAT 318 Data Cleaning.R
@@ -1,13 +1,13 @@
 ## Read in data
-happiness.dat = read.csv('C:/Users/eliza/Downloads/STAT 318 Data Sets/Happiness2018.csv', header = TRUE)
-gdi.dat = read.csv('C:/Users/eliza/Downloads/STAT 318 Data Sets/GDI2019.csv',  header = TRUE)
+happiness.dat = read.csv('C:/Users/eliza/stat318-git/stat318-project/data_files/Happiness2018.csv', header = TRUE)
+gdi.dat = read.csv('C:/Users/eliza/stat318-git/stat318-project/data_files/GDI2019.csv',  header = TRUE)
 gdi.dat = gdi.dat[-1,] # Remove extra header row
-maternal.dat = read.csv('C:/Users/eliza/Downloads/STAT 318 Data Sets/Maternal2017.csv', header = TRUE, skip=1)
-lifexF.dat = read.csv('C:/Users/eliza/Downloads/STAT 318 Data Sets/LifeExF2018.csv', header = TRUE, skip=1)
-lifexM.dat = read.csv('C:/Users/eliza/Downloads/STAT 318 Data Sets/LifeExM2018.csv', header = TRUE, skip=1)
-politics.dat = read.csv('C:/Users/eliza/Downloads/STAT 318 Data Sets/Politics20172019.csv', header = TRUE)
-
-schooling.dat = read.csv('C:/Users/eliza/Downloads/STAT 318 Data Sets/Schooling2019.csv', header = TRUE)
+maternal.dat = read.csv('C:/Users/eliza/stat318-git/stat318-project/data_files/Maternal2017.csv', header = TRUE, skip=1)
+lifexF.dat = read.csv('C:/Users/eliza/stat318-git/stat318-project/data_files/LifeExF2018.csv', header = TRUE, skip=1)
+lifexM.dat = read.csv('C:/Users/eliza/stat318-git/stat318-project/data_files/LifeExM2018.csv', header = TRUE, skip=1)
+politics.dat = read.csv('C:/Users/eliza/stat318-git/stat318-project/data_files/Politics20172019.csv', header = TRUE)
+schooling.dat = read.csv('C:/Users/eliza/stat318-git/stat318-project/data_files/Schooling2019.csv', header = TRUE)
+employment.dat = read.csv('C:/Users/eliza/stat318-git/stat318-project/data_files/EmploymentDiscrimination2018.csv', header=TRUE)
 
 ## Create data frame
 happiness_scores = data.frame(happiness.dat$Score, happiness.dat$Country.or.region)
@@ -31,6 +31,8 @@ colnames(maternal_scores) = c('Maternal Mortality (Per 100,000 Births)','Country
 schooling_scores = data.frame(schooling.dat$Excep_Yrs_Schooling_Diff, schooling.dat$Country)
 colnames(schooling_scores) = c('Length of Schooling (Years)(Female-Male)','Country')
 
+employment_scores = data.frame(employment.dat$Value, employment.dat$Country.Name)
+colnames(employment_scores) = c('Employment Discrimination Illegal', 'Country')
 
 ## Join data sets
 library(dplyr)
@@ -40,16 +42,17 @@ fullJoinDf2 = full_join(fullJoinDf1, lifeex_scores, by="Country")
 fullJoinDf3 = full_join(fullJoinDf2, politics_scores, by="Country")
 fullJoinDf4 = full_join(fullJoinDf3, maternal_scores, by="Country")
 fullJoinDf5 = full_join(fullJoinDf4, schooling_scores, by="Country")
-### MORE VARIABLES TO BE ADDED
+fullJoinDf6 = full.join(fullJoinDf5, employment_scores, by="Country")
 
 ## Export
-attach(fullJoinDf5)
+attach(fullJoinDf6)
 final.dat = data.frame(Country, `Happiness Score`, `GDI Value`,
                         `Life Expectancy Difference (Female - Male)`, `Female Parlementarians (%)`, 
-                        `Maternal Mortality (Per 100,000 Births)`, `Length of Schooling (Years)(Female-Male)`)
-detach(fullJoinDf5)
+                        `Maternal Mortality (Per 100,000 Births)`, `Length of Schooling (Years)(Female-Male)`, 
+                       `Employment Discrimination Illegal`)
+detach(fullJoinDf6)
 
 colnames(final.dat) = c('Country','HappinessScore','GDI','LifeExDiff','FemaleParliament',
-                        'MaternalMort','SchoolingDiff')
+                        'MaternalMort','SchoolingDiff', 'EmployDiscrimination')
 
-write.csv(final.dat, 'C:/Users/eliza/Downloads/STAT 318 Data Sets/FinalData.csv')
+write.csv(final.dat, 'C:/Users/eliza/stat318-git/stat318-project/FinalData.csv')
